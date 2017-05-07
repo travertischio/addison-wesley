b3038ecdf1c4259364e4f98c633f2d700bdba68b
travertischio <travertischio@gmail.com>
2017-05-07 16:47:55 -0400

    Benchmarks shift pop count and tests all

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/travertischio/addison-wesley/popcount"
+	"os"
 	"testing"
 )
 
@@ -16,5 +18,35 @@ func BenchmarkPopCountSingle(b *testing.B) {
 func BenchmarkPopCountLoop(b *testing.B) {
 	for i := 0; i < 10000000; i++ {
 		count = popcount.PopCountLoop(uint64(i))
+	}
+}
+
+func BenchmarkPopCountShift(b *testing.B) {
+	for i := 0; i < 10000000; i++ {
+		count = popcount.PopCountShift(uint64(i))
+	}
+}
+
+func TestDoTheseWorkSingle(t *testing.T) {
+	test := popcount.PopCountSingle(uint64(29))
+	if 4 != test {
+		fmt.Printf("%v", test)
+		os.Exit(1)
+	}
+}
+
+func TestDoTheseWorkLoop(t *testing.T) {
+	test := popcount.PopCountLoop(uint64(29))
+	if 4 != test {
+		fmt.Printf("%v", test)
+		os.Exit(1)
+	}
+}
+
+func TestDoTheseWorkShift(t *testing.T) {
+	test := popcount.PopCountShift(uint64(29))
+	if 4 != test {
+		fmt.Printf("%v", test)
+		os.Exit(1)
 	}
 }
